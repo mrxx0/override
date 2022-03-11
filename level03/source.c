f1e3f4b361fb0b8385a5392f3bfee08fecb5e288
Florian HEMBERT <flhember@e1r10p9.42.fr>
2022-03-11 13:52:30 +0100

    update source.c and tmp

--- level03/source.c
+++ level03/source.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
-void 	test(int pass, int val)
+void		decrypt(int val)
 {
-	printf("%d %d", pass, val);
+	char 	str[] = "Q}|u`sfg~sf{}|a3";
+	int		i = 0;
+	int		j = 0;
 
+	while (str[i] != '\0')
+		i++;
+	while (j < i ) {
+		char c = str[j] ^ val;
+		str[j] = c;
+		j++;
+	}
+	printf("%s \n", str);
+	if (!strncmp(str, "Congratulations!", 17)) {
+		system("/bin/sh");
+	} else {
+		printf("\nInvalid Password\n");
+	}
+}
+
+void 		test(int pass, int val)
+{
+	val = val - pass;
+	if (val == 0x15) {
+		int decalage = val << 2;
+		decalage += 134515184;
+		//jmp louche
+		decrypt(val);
+	} else {
+		decrypt(rand());
+	}
 }
 
 int		main(void)
 {
 		int password;
 
-		srand(time(0));
+		srand(time(NULL));
 		printf("***********************************\n");
 		printf("*		level03		**\n");
 		printf("***********************************\n");
