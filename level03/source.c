7321cd6ba266ceee05b579a41a458b183b5c8df8
charlesctr <chcoutur@student.42.fr>
2022-03-11 17:13:29 +0100

    add switch case for source level03

--- level03/source.c
+++ level03/source.c
@@ -16,7 +16,6 @@ void		decrypt(int val)
 		str[j] = c;
 		j++;
 	}
-	printf("%s \n", str);
 	if (!strncmp(str, "Congratulations!", 17)) {
 		system("/bin/sh");
 	} else {
@@ -27,26 +26,86 @@ void		decrypt(int val)
 void 		test(int pass, int val)
 {
 	val = val - pass;
-	if (val == 0x15) {
-		int decalage = val << 2;
-		decalage += 134515184;
-		//jmp louche
-		decrypt(val);
-	} else {
-		decrypt(rand());
+	switch(val)
+	{
+		case 0x1:
+			decrypt(val);
+			break;
+		case 0x2:
+			decrypt(val);
+			break;
+		case 0x3:
+			decrypt(val);
+			break;
+		case 0x4:
+			decrypt(val);
+			break;
+		case 0x5:
+			decrypt(val);
+			break;
+		case 0x6:
+			decrypt(val);
+			break;
+		case 0x7:
+			decrypt(val);
+			break;
+		case 0x8:
+			decrypt(val);
+			break;
+		case 0x9:
+			decrypt(val);
+			break;
+		case 0xA:
+			decrypt(val);
+			break;
+		case 0xB:
+			decrypt(val);
+			break;
+		case 0xC:
+			decrypt(val);
+			break;
+		case 0xD:
+			decrypt(val);
+			break;
+		case 0xE:
+			decrypt(val);
+			break;
+		case 0xF:
+			decrypt(val);
+			break;
+		case 0x10:
+			decrypt(val);
+			break;
+		case 0x11:
+			decrypt(val);
+			break;
+		case 0x12:
+			decrypt(val);
+			break;
+		case 0x13:
+			decrypt(val);
+			break;
+		case 0x14:
+			decrypt(val);
+			break;
+		case 0x15:
+			decrypt(val);
+			break;
+		default:
+			decrypt(rand());
 	}
 }
 
 int		main(void)
 {
-		int password;
+	int password;
 
-		srand(time(NULL));
-		printf("***********************************\n");
-		printf("*		level03		**\n");
-		printf("***********************************\n");
-		printf("Password:");
-		scanf("%d", &password);
-		test(password, 0x1337d00d);
-		return (0);
+	srand(time(NULL));
+	printf("***********************************\n");
+	printf("*		level03		**\n");
+	printf("***********************************\n");
+	printf("Password:");
+	scanf("%d", &password);
+	test(password, 0x1337d00d);
+	return (0);
 }
